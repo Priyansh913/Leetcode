4e04baabf9462487c0c685d1a4bd05a2359e967a
Priyansh913 <priyanshpatel791@gmail.com>
2026-07-31 22:08:28 +0530

    Time: 8 ms (80.85%), Space: 13.7 MB (16.66%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -5,38 +5,39 @@ class Solution(object):
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
 
+        row, col = len(matrix), len(matrix[0])
         first_row_zero, first_col_zero = 0, 0
 
-        for i in range(len(matrix)):
+        for i in range(row):
             if matrix[i][0] == 0:
                 first_col_zero = True
 
-        for i in range(len(matrix[0])):
+        for i in range(col):
             if matrix[0][i] == 0:
                 first_row_zero = True
 
-        for i in range(1, len(matrix)):
-            for j in range(1, len(matrix[0])):
+        for i in range(1, row):
+            for j in range(1, col):
                 if matrix[i][j] == 0:
                     matrix[i][0] = 0
                     matrix[0][j] = 0
 
-        for i in range(1, len(matrix)):
+        for i in range(1, row):
             if matrix[i][0] == 0:
-                matrix[i] = [0] * len(matrix[0])
+                matrix[i] = [0] * col
 
-        for i in range(1, len(matrix[0])):
+        for i in range(1, col):
             if matrix[0][i] == 0:
-                for j in range(1, len(matrix)):
+                for j in range(1, row):
                     matrix[j][i] = 0
 
         if first_row_zero:
             matrix[0] = [0 for _ in range(len(matrix[0]))]
 
         if first_col_zero:
-            for i in range(len(matrix)):
+            for i in range(row):
                 matrix[i][0] = 0
-                         
+
 matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
 sol = Solution()
 sol.setZeroes(matrix)
